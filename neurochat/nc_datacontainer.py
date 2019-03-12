84d36c522ca880359d8a82c35ae5d1f8e1087353
Sean Martin <SMARTIN5@tcd.ie>
2019-03-12 13:49:59 +0000

    Initial version working for just spikes

--- neurochat/nc_datacontainer.py
+++ neurochat/nc_datacontainer.py
@@ -5,6 +5,9 @@ This module implements a container for the Ndata class to simplify multi experim
 @author: Sean Martin; martins7 at tcd dot ie
 """
 
+from enum import Enum
+import logging
+
 from neurochat.nc_data import NData
 
 # Ideas - set up a file class which stores where the filenames are
@@ -22,9 +25,8 @@ from neurochat.nc_data import NData
 
 # Loading from excel file
 
-
 class NDataContainer():
-    def __init__(self, LoadOnFly=True):
+    def __init__(self, load_on_fly=True):
         """
         Parameters
         ----------
@@ -38,12 +40,88 @@ class NDataContainer():
         file_names : List
         
         """
-        self.file_names = []
-        self.container = []
+        self._load_on_fly = load_on_fly
+        self._file_names_dict = {}
+        self._container = []
 
-    def add(self, data):
-        container.append(data)
+    class EFileType(Enum):
+        Spike = 1
+        Position = 2
+        LFP = 3
+        HDF = 4
 
-    def add_file(self)
+    def get_num_data(self):
+        return len(self._container)
+    
+    def get_file_dict(self):
+        return self._file_names_dict
 
-    def load_from_excel(self, )
+    def add_data(self, data):
+        if isinstance(data, NData):
+            self._container.append(data)
+        else:
+            logging.error("Adding incorrect object to data container")
+            return
+
+    def get_data(self, index):
+        if index >= self.get_num_data():
+            logging.error("Input index to get_data out of range")
+            return
+        return self._container[index]
+
+    def add_files(self, f_type, descriptors):
+        filenames, _, _ = descriptors
+        if not isinstance(f_type, self.EFileType):
+            logging.error("Parameter f_type in add files must be of EFileType")
+            return
+
+        # Ensure lists are empty or of equal size    
+        for l in descriptors:
+            if l is not None:
+                if len(l) != len(filenames):
+                    logging.error(
+                        "add_files called with differing number of filenames and other data"
+                    )
+                    return
+
+        for idx in range(len(filenames)):
+            description = []
+            for el in descriptors:
+                if el is not None:
+                    description.append(el[idx])
+                else:
+                    description.append(None)
+            self._file_names_dict.setdefault(
+                f_type.name, []).append(description)
+
+    def _load(self, ndata, key, descriptor):
+        key_fn_pairs = {
+            "Spike" : [
+                getattr(ndata, "set_spike_file"), 
+                getattr(ndata, "set_spike_name")]
+        }
+
+        filename, objectname, system = descriptor
+
+        if objectname is not None:
+            key_fn_pairs[key][1](objectname)
+
+        if system is not None:
+            ndata.set_system(system)
+
+        if filename is not None:
+            key_fn_pairs[key][0](filename)
+            ndata.load_spike()
+
+    def load_all_data(self):
+        for key, vals in self.get_file_dict().items():
+            for idx, _ in enumerate(vals):
+                if idx >= self.get_num_data():
+                    self.add_data(NData())
+            
+            for idx, descriptor in enumerate(vals):
+                self._load(
+                    self.get_data(idx), key, descriptor)
+
+    def add_files_from_excel(self, file_loc):
+        pass
