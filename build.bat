ac81ee905a82e0f2701500c63e2b4b8c8d9cefb7
Sean Martin <SMARTIN5@tcd.ie>
2019-03-15 12:21:42 +0000

    Update to build installer_requirements downgraded numpy Exe builds but not tested

--- build.bat
+++ build.bat
@@ -1,8 +1,10 @@
 @echo off
-C:/Users/smartin5/AppData/Local/Programs/Python/Python37/python.exe -m PyInstaller --noconfirm --log-level=WARN ^
+C:/Users/smartin5/AppData/Local/Programs/Python/Python37/python.exe -m PyInstaller ^
+    --noconfirm --log-level WARN ^
     --onefile --nowindow ^
-    --hidden-import=xlrd ^
-    --hidden-import=openpyxl ^
-    --name=NeuroChaT ^
-    --icon=NeuroChat ^
-    cli.spec
+    --hidden-import xlrd ^
+    --hidden-import openpyxl ^
+    --name NeuroChaT ^
+    --icon NeuroChaT.ico ^
+    --additional-hooks-dir=hooks ^
+    cli.py
