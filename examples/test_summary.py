2294acb6818310d1a7d7326d7cae2503908c568e
Sean Martin <martins7@tcd.ie>
2019-05-10 15:56:05 +0100

    Updated example file

--- examples/test_summary.py
+++ examples/test_summary.py
@@ -4,10 +4,13 @@ sys.path.insert(1, r'C:\Users\smartin5\Repos\myNeurochat')
 from neurochat.nc_datacontainer import NDataContainer
 import neurochat.nc_containeranalysis as nca
 
+
 def main(dir):
     container = NDataContainer(load_on_fly=True)
     container.add_axona_files_from_dir(dir)
+    container.setup()
     nca.place_cell_summary(dir, container)
+
 
 if __name__ == "__main__":
     dir = r'C:\Users\smartin5\recording_example'
