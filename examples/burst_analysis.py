671233b2e0069131f69d6aacc333740e1f8e34f8
Sean Martin <martins7@tcd.ie>
2019-06-10 10:29:52 +0100

    Fixed burst analysis script

--- examples/burst_analysis.py
+++ examples/burst_analysis.py
@@ -3,21 +3,23 @@ sys.path.insert(1, r'C:\Users\smartin5\Repos\myNeurochat')
 
 import csv
 import os
+from copy import copy
 
 
 from neurochat.nc_datacontainer import NDataContainer
 from neurochat.nc_containeranalysis import place_cell_summary
-from neurochat.nc_utils import make_dir_if_not_exists, log_exception
+from neurochat.nc_utils import make_dir_if_not_exists, log_exception, oDict
 
 
 # TODO could do many in this
-def save_results_to_csv(filename, in_dict):
-    names = in_dict.keys()
+def save_results_to_csv(filename, in_dicts):
+    names = in_dicts[0].keys()
     try:
         with open(filename, 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=names)
             writer.writeheader()
-            writer.writerow(in_dict)
+            for in_dict in in_dicts:
+                writer.writerow(in_dict)
 
     except Exception as e:
         log_exception(e, "When {} saving to csv".format(filename))
@@ -29,21 +31,31 @@ def main(in_dir, tetrode_list, analysis_flags):
     container.add_axona_files_from_dir(in_dir, tetrode_list=tetrode_list)
     container.setup()
     spike_names = container.get_file_dict()["Spike"]
+
     # show summary of place
     if analysis_flags[0]:
         place_cell_summary(container)
-    # # do burst analysis
-    # if analysis_flags[1]:
-    out_dir = os.path.join(in_dir, "nc_results")
-    for i, ndata in enumerate(container):
-        iter_num = container._index_to_data_pos(i)
-        name = spike_names[iter_num[0]][0]
-        out_name = os.path.join(out_dir, name) + "_" + \
-            str(iter_num[1]) + ".csv"
-        make_dir_if_not_exists(out_name)
-        ndata.burst()
-        print(ndata.get_results())
-        save_results_to_csv(out_name, ndata.get_results())
+
+    # do burst analysis
+    if analysis_flags[1]:
+        _results = []
+        out_dir = os.path.join(in_dir, "nc_results")
+        for i, ndata in enumerate(container):
+            split_idx = container._index_to_data_pos(i)
+            name = spike_names[split_idx[0]][0]
+            parts = os.path.basename(name).split(".")
+            end_name = parts[0] + "_burst" + ".csv"
+            out_name = os.path.join(
+                out_dir, end_name)
+            make_dir_if_not_exists(out_name)
+            note_dict = oDict()
+            note_dict["Tetrode"] = int(parts[1])
+            note_dict["Unit"] = ndata.get_unit_no()
+            ndata.update_results(note_dict)
+            ndata.burst()
+            result = copy(ndata.get_results())
+            _results.append(result)
+        save_results_to_csv(out_name, _results)
 
     # # can ignore above if multi run
     # if analysis_flags[2]:
