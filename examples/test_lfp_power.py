09d308457f608f31c88f4e0f5a32da015817c7fd
Sean Martin <martins7@tcd.ie>
2019-07-22 17:02:38 +0100

    Added argument parsing to test lfp

--- examples/test_lfp_power.py
+++ examples/test_lfp_power.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 sys.path.insert(1, r'C:\Users\smartin5\Repos\myNeurochat')
 from load_example_data import load_data
 
@@ -23,37 +24,40 @@ def plot_lfp_signal(lfp, lower, upper, out_name, filt=True):
     plt.savefig(out_name)
 
 
-def lfp_power(new_data, i, max_f, prefilt=False):
+def lfp_power(new_data, i, max_f, prefilt=False, relative=True):
     # 1.6 or 2 give similar
-    filtset = [10, 1, max_f, 'bandpass']
+    filtset = [10, 1.5, max_f, 'bandpass']
 
     new_data.bandpower_ratio(
-        [5, 11], [1.5, 4], 2, relative=True, prefilt=prefilt,
+        [5, 11], [1.5, 4], 1.6, relative=relative, prefilt=prefilt,
         first_name="Theta", second_name="Delta",
         filtset=filtset)
     print("For {} results are {}".format(i, new_data.get_results()))
 
     graphData = new_data.spectrum(
-        window=2, noverlap=1, nfft=500, ptype='psd', prefilt=prefilt,
+        window=1.6, noverlap=1, nfft=1000, ptype='psd', prefilt=prefilt,
         filtset=filtset, fmax=max_f, db=False, tr=False)
     fig = nc_plot.lfp_spectrum(graphData)
     fig.savefig("spec" + str(i) + ".png")
     graphData = new_data.spectrum(
-        window=2, noverlap=1, nfft=500, ptype='psd', prefilt=prefilt,
+        window=1.6, noverlap=1, nfft=1000, ptype='psd', prefilt=prefilt,
         filtset=filtset, fmax=max_f, db=True, tr=True)
     fig = nc_plot.lfp_spectrum_tr(graphData)
     fig.savefig("spec_tr" + str(i) + ".png")
     plt.close("all")
 
 
-def main(ndata, analysis_flags, max_lfp=40):
+def main(ndata, analysis_flags, parsed):
+    max_lfp = parsed.max_freq
+    print(max_lfp)
+    filt = parsed.prefilt
+    relative = parsed.relative
     if analysis_flags[0]:
         plot_lfp_signal(
-            ndata.lfp, 0, max_lfp, "full_signal.png", filt=False)
+            ndata.lfp, 1.5, max_lfp, "full_signal.png", filt=filt)
 
     if analysis_flags[1]:
-        fs = ndata.lfp.get_sampling_rate()
-        lfp_power(ndata, 4, max_lfp, prefilt=True)
+        lfp_power(ndata, 4, max_lfp, prefilt=True, relative=relative)
         splits = [
             (0, 900),
             (900, 1800)]
@@ -61,10 +65,23 @@ def main(ndata, analysis_flags, max_lfp=40):
 
         for i, split in enumerate(splits):
             new_data = ndata.subsample(split)
-            lfp_power(new_data, i, max_lfp, prefilt=True)
+            lfp_power(new_data, i, max_lfp, prefilt=True, relative=relative)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parse a program location")
+    parser.add_argument(
+        "--prefilt", "-pf", action="store_true",
+        help="Should pre filter lfp before power and spectral analysis")
+    parser.add_argument(
+        "--relative", "-r", action="store_true",
+        help="Should band power be relative to total signal power"
+    )
+    parser.add_argument(
+        "--max_freq", "-mf", type=int, default=40,
+        help="The maximum lfp frequency to consider"
+    )
+    parsed = parser.parse_args()
 
     # print("Dealing with nt\n")
     # in_dir = r'C:\Users\smartin5\Recordings\ER\26062019-nt'
@@ -94,4 +111,4 @@ if __name__ == "__main__":
     file = os.path.join(in_dir, lfp_file)
     ndata = NData()
     ndata.lfp.load(file)
-    main(ndata, [True, True], 125)
+    main(ndata, [True, True], parsed)
